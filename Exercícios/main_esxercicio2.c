eee57543a9ceca1ee3133e7dc9450e1bc7d01016
Jorge-9411 <jorgegameplay.pessoal@gmail.com>
2026-08-13 16:33:18 -0300

    Add files via upload

---
+++ Exercícios/main_esxercicio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int contador = 10;
+	while (contador >= 1){
+		printf("i%\n", contador);
+		contador--;
+	}
+	return 0;
+}
